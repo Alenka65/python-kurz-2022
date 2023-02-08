a4545bff05b28db85ed1fbeff2729d7435e80a75
Alena Rudykh <rudykh.alena@gmail.com>
2023-02-08 18:35:57 +0100

    Ukol3

---
+++ Ukol_3.py
@@ -0,0 +1,18 @@
+import json
+with open('body.json', encoding='utf-8') as soubor:
+    body = json.load(soubor)
+print(body)
+
+for student, points in body.items(): 
+    if points >= 60: 
+        body[student] = 'Pass'
+    
+    else:
+        body[student] = 'Fail'
+        
+print(body)
+
+import json
+with open('body2.json', mode='w', encoding='utf-8') as soubor:
+    json.dump(body, soubor)
+
