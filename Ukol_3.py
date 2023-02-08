624d1ccacc55e1983c9ed1c08b59f040d9c1ac9a
Alena Rudykh <rudykh.alena@gmail.com>
2023-02-08 19:07:02 +0100

    Ukol3 - oprava

--- Ukol_3.py
+++ Ukol_3.py
@@ -13,7 +13,7 @@ for student, points in body.items():
 print(body)
 
 import json
-with open('body2.json', mode='w', encoding='utf-8') as soubor:
-    json.dump(body, soubor)
+with open('body3.json', mode='w', encoding='utf-8') as soubor:
+    json.dump(body, soubor, ensure_ascii=False, indent=4)
 
 #Mohl byste mi poradit proc body2.json file je uplne rozbity kvuli diakritice? Netusim co s tim mam udelat :(
